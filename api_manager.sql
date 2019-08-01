e55bd78469231df38f7eccef54ce05f8afffb98f
dmsansan <562404326@qq.com>
2019-08-01 17:51:21 +0800

    修改数据库脚本

--- api_manager.sql
+++ api_manager.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50722
  File Encoding         : 65001
 
- Date: 22/07/2019 15:39:32
+ Date: 01/08/2019 17:49:54
 */
 
 SET NAMES utf8mb4;
@@ -22,18 +22,19 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `api_user`;
 CREATE TABLE `api_user`  (
-  `id` int(11) NOT NULL,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_name` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `pass_word` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `add_time` timestamp(0) NULL DEFAULT NULL,
   `last_update_time` timestamp(0) NULL DEFAULT NULL,
   `delete_status` int(1) NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of api_user
 -- ----------------------------
 INSERT INTO `api_user` VALUES (1, 'admin', 'admin', '2019-07-22 15:37:05', '2019-07-22 15:37:05', 0);
+INSERT INTO `api_user` VALUES (2, 'custom', 'custom', '2019-07-23 16:31:21', '2019-07-23 16:31:24', 0);
 
 SET FOREIGN_KEY_CHECKS = 1;
